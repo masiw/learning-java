537012f6c505a33aac3b853ab56fe551f5985a2b
masi <masi.woermann@gmail.com>
2020-10-28 21:37:34 +0000

    Who likes it - Masi

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import codeWars.WhoLikesIt;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println(WhoLikesIt.whoLikesIt(args));
 	}
 
 }
