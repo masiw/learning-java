fca0add4fa7785893946fd549ae632922f910992
masi <masi.woermann@gmail.com>
2020-10-28 21:48:09 +0000

    Yes or No

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import codeWars.WhoLikesIt;
+import codeWars.YesOrNo;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(WhoLikesIt.whoLikesIt(args));
+		
+		System.out.println(String.format("%s or %s", YesOrNo.boolToWord(true), YesOrNo.boolToWord(false)));
 	}
 
 }
