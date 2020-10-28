489499b17b285165bf25a3793f86f265e87e4243
masi <masi.woermann@gmail.com>
2020-10-28 21:41:49 +0000

    Who likes it - improvement

--- src/codeWars/WhoLikesIt.java
+++ src/codeWars/WhoLikesIt.java
@@ -1,19 +1,13 @@
 package codeWars;
 
 public class WhoLikesIt {
-    public static String whoLikesIt(String... names) {
-        if (names.length == 0) {
-        	return "no one likes this";
-        }
-        if (names.length == 1) {
-        	return names[0] + " likes this";
-        }
-        if (names.length == 2) {
-        	return names[0] + " and " + names[1] + " like this";
-        }
-        if (names.length == 3) {
-            return names[0] + ", " + names[1] + " and " + names[0] + " like this";
-        }
-        return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
-    } 
+	public static String whoLikesIt(String... names) {
+	    switch (names.length) {
+	    case 0: return "no one likes this";
+	    case 1: return String.format("%s likes this", names[0]);
+	    case 2: return String.format("%s and %s like this", names[0], names[1]);
+	    case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+	    default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+	  }
+	} 
 }
