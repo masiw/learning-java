b06a7bfe59de1294c08d5052c9e2958e1d740541
masi <masi.woermann@gmail.com>
2020-10-30 23:10:30 +0000

    CodeWars - 578553c3a1b8d5c40300037c - Binary to number - Improvement

--- src/codeWars/BinaryArrayToNumber.java
+++ src/codeWars/BinaryArrayToNumber.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public class BinaryArrayToNumber {
   public static int ConvertBinaryArrayToInt(List<Integer> binary) {
-    int value = 0;
-    for (int i = 0; i < binary.size(); i++) {
-      value += Math.pow(2, i) * binary.get(binary.size() - i - 1);
-    }
-    return value;
+    return binary.stream().reduce((x, y) -> x * 2 + y).get();
   }
 }
