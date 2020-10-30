05bf53064ad944bd21e7fdddcc0b7f752f0a003d
masi <masi.woermann@gmail.com>
2020-10-30 21:58:26 +0000

    CodeWars - 5648b12ce68d9daa6b000099 - Metro - Improvement

--- src/codeWars/Metro.java
+++ src/codeWars/Metro.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Metro {
   public static int countPassengers(List<int[]> stops) {
     return stops.stream()
-      .map(stop -> stop[0] - stop[1])
-      .reduce(0, (subtotal, element) -> subtotal + element);
+      .mapToInt(stop -> stop[0] - stop[1])
+      .sum();
   }
 }
