ae9e3618b17364f6ff031a105d32a5168be092e3
masi <masi.woermann@gmail.com>
2020-11-01 19:07:43 +0000

    CodeWars - 556deca17c58da83c00002db - Tribonacci - improvement - fixes

--- src/codeWars/Xbonacci.java
+++ src/codeWars/Xbonacci.java
@@ -6,7 +6,7 @@ public class Xbonacci {
 
   public double[] tribonacci(double[] s, int n) {
     double[] result = Arrays.copyOf(s, n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 3; i < n; i++) {
       result[i] = result[i-3] + result[i-2] + result[i-1];
     }
     return result;
