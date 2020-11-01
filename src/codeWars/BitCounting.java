5f37e21c317d41a47bc174b8ae906c465d0ccb38
masi <masi.woermann@gmail.com>
2020-11-01 21:29:24 +0000

    CodeWars - 526571aae218b8ee490006f4 - Bit counting - improvement

--- src/codeWars/BitCounting.java
+++ src/codeWars/BitCounting.java
@@ -3,13 +3,7 @@ package codeWars;
 public class BitCounting {
 
   public static int countBits(int n){
-    int count = 0;
-    for (int i = 0; Math.pow(2, i) <= n; i++) {
-      if (0 < (n & (int)Math.pow(2, i))) {
-        count++;
-      }
-    }
-    return count;
+    return Integer.bitCount(n);
   }
 
 }
