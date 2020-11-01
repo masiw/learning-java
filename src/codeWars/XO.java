ad3a03e9d859644b40ddccbb4fd14f58f6858075
masi <masi.woermann@gmail.com>
2020-11-01 19:16:10 +0000

    CodeWars - 55908aad6620c066bc00002a - Exes and Ohs - improvement

--- src/codeWars/XO.java
+++ src/codeWars/XO.java
@@ -3,17 +3,6 @@ package codeWars;
 public class XO {
   
   public static boolean getXO (String str) {
-    int count = 0;
-    for (int i = 0; i < str.length(); i++) {
-      char current = str.charAt(i);
-      if (current == 'x' || current == 'X') {
-        count += 1;
-      }
-      else if (current == 'o' || current == 'O') {
-        count -= 1;
-      }
-    }
-    return count == 0;
-    
+    return str.replaceAll("[oO]", "").length() == str.replaceAll("[xX]", "").length();
   }
 }
